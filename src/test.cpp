c2ece56938b2eb154d97249c92d2b1f5b22afd5c
madeofparts <190052804+madeofparts@users.noreply.github.com>
2026-08-10 21:16:58 -0700

    Add function to get filepath from user

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,32 @@
-
 #include <iostream>
+#include <filesystem>
+#include <string>
+#include <format>
+
+namespace fs = std::filesystem;
+
+// TODO: automatically insert / at start if missing
+std::string get_input_file_path() {
+  fs::path my_path;
+
+  while (!fs::exists(my_path)) {
+    std::string input;
+    std::cout << "Select downloads directory: ";
+    std::getline(std::cin, input);
+    my_path = input;
+
+    if (fs::exists(my_path)) {
+      std::cout << std::format("Path \'{}\' exists.", my_path.string()) << std::endl;
+    } else {
+      std::cout << std::format("Path \'{}\' does not exist. Please try again.", my_path.string()) << std::endl; 
+    }
+  }
+
+  return my_path;
+}
 
 int main() {
-  std::cout << "Hello, world." << std::endl;
+  get_input_file_path();  
   return 0;
 }
 
