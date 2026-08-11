c23d28b0b9d037f36ebc252686be1d29f1fc4f90
madeofparts <190052804+madeofparts@users.noreply.github.com>
2026-08-11 15:12:06 -0700

    Add confirmation and improve text for get_input_file_path

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <filesystem>
 #include <string>
@@ -5,20 +6,35 @@
 
 namespace fs = std::filesystem;
 
-// TODO: 
-// - automatically insert / at start if missing
-// - add confirm message
-std::string get_input_file_path(std::string purpose) {
-  fs::path my_path; //
+std::string get_input_file_path(std::string purpose = "") {
+  fs::path my_path;
 
   while (!fs::exists(my_path)) {
     std::string input;
-    std::cout << std::format("Select {} directory: ", purpose);
+    std::string msg;
+
+    if (purpose == "") {
+      msg = "Select directory: ";
+    } else {
+      msg = "Select {} directory: ";
+    }
+    std::cout << std::vformat(msg, std::make_format_args(purpose));
     std::getline(std::cin, input);
     my_path = input;
 
     if (fs::exists(my_path)) {
       std::cout << std::format("Path \'{}\' exists.", my_path.string()) << std::endl;
+      std::cout << std::format("Confirm path: \'{}\' (\033[32mY\033[0m/\033[31mn\033[0m): ", my_path.string());
+      std::string ans;
+      std::getline(std::cin, ans);
+      
+      std::transform(ans.begin(), ans.end(), ans.begin(), ::tolower);
+      if (ans == "y" || ans == "yes" || ans == "") {
+        std::cout << "Confirmed." << std::endl;
+      } else {
+        std::cout << "Cancelled." << std::endl;
+        my_path = "";
+      }
     } else {
       std::cout << std::format("Path \'{}\' does not exist. Please try again.", my_path.string()) << std::endl; 
     }
@@ -28,7 +44,8 @@ std::string get_input_file_path(std::string purpose) {
 }
 
 int main() {
-  get_input_file_path("test");  
+  get_input_file_path();
+  get_input_file_path("test");
   return 0;
 }
 
